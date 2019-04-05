09e30fbb01b89f5e8fc006596093ce6cefff82db
Spencer Hughes <16389711+spencerhughes@users.noreply.github.com>
2019-04-05 10:36:22 -0400

    Tidies up extract_tar function

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import glob, os, zipfile, tarfile
 
-### FUNCTIONS
+### BEGINNING OF FUNCTIONS
 
 def extract_tar(tar_file_name, output_dir):
-	tar = tarfile.open(tar_file_name, "r:gz")
-	tar.extractall(output_dir)
-	tar.close()
+	with tarfile.open(tar_file_name,"r:gz") as tar_file:
+		tar_file.extractall(output_dir)
 	return
 
 def extract_zip(zip_file_name, output_dir):
@@ -14,9 +13,11 @@ def extract_zip(zip_file_name, output_dir):
 	return
 
 ### END OF FUNCTIONS
+### BEGINNING OF APP
 takeout_dir = input("Please input the path to your Google Takeout archive: ")
 for filename in glob.glob(os.path.join(takeout_dir, 'takeout-*')):
 	if (filename.endswith("tgz")):
 		extract_tar(filename, takeout_dir)
 	elif (filename.endswith("zip")):
 		extract_zip(filename, takeout_dir)
+### END OF APP
